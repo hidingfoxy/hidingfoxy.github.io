7c7d6f2042cc077a05a4c2fa87a09a3bc8e36e33
EF <70142324+eldrfoa@users.noreply.github.com>
2024-06-30 23:44:25 +0500

    Redesign of 2024
    finally huh?

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 function tab(idnum) {
-	console.log(idnum)
 	tabscontent = document.getElementsByClassName("onetab");
-	console.log(tabscontent)
 	for (i=0; i<tabscontent.length; i++) {
 		if (idnum == i) {
 			document.getElementById("Tab" + i.toString()).classList.add("tab-active")
@@ -14,7 +12,55 @@ function tab(idnum) {
 	}
 }
 
-var page1 = document.getElementById("main-block");
-function scrollto1() {
-	page1.scrollIntoView({behavior: "smooth"});
+
+var moon = 0;
+var tabslist = document.querySelectorAll('.onetab');
+
+function NightMode() {
+	if (moon === 0) {
+		moon = 1;
+		document.getElementById("body").setAttribute("class", "dark-mode");
+		document.getElementById("moon").setAttribute("src", "images/darkmoon.png")
+		for(var i = 0; i < tabslist.length; i++) {
+			tabslist[i].classList.toggle("dark-card");
+		  }
+	} else {
+		moon = 0;
+		document.getElementById("body").setAttribute("class", "");
+		document.getElementById("moon").setAttribute("src", "images/lightmoon.png")
+		for(var i = 0; i < tabslist.length; i++) {
+			tabslist[i].classList.toggle("dark-card");
+		  }
+	}
+	
+} 
+
+
+function contopen(elem) {
+	document.getElementById(elem.id+'w').style.display = "flex";
+	elem.parentElement.parentElement.parentElement.parentElement.style.display = "none";
+	document.body.scrollTop = 0;
 }
+
+function contclose(elem) {
+	elem.parentElement.parentElement.parentElement.firstElementChild.style.display = "block";
+	elem.parentElement.parentElement.style.display = "none";
+	
+}
+
+
+
+var modal = document.getElementById("myModal");
+var modalImg = document.getElementById("modalimg");
+var captionText = document.getElementById("caption");
+var span = document.getElementsByClassName("close")[0];
+
+function modalopen(img){
+	modal.style.display = "block";
+	modalImg.src = img.src;
+	captionText.innerHTML = img.alt;
+}
+
+span.onclick = function() {
+	modal.style.display = "none";
+} 
